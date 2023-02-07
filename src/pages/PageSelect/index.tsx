076a0333930483e0d0b0d75df923edda2a00fc50
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-02-07 19:46:55 -0300

    feat: select working

--- src/pages/PageSelect/index.tsx
+++ src/pages/PageSelect/index.tsx
@@ -1,5 +1,17 @@
+import { Select } from "@/src/components/Select";
 import React from "react";
 
 export const PageSelect = () => {
-  return <div>PageSelect</div>;
+  return (
+    <div>
+      <Select
+        options={[
+          { label: "Option 1", value: "opt1" },
+          { label: "Option 2", value: "opt2" },
+          { label: "Option 3", value: "opt3" },
+        ]}
+        value={"opt2"}
+      />
+    </div>
+  );
 };
